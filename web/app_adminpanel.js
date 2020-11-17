60a6367ef82a909a347649fc0203f40e8b4bf3d6
Dardan Isufi <dardan.isufi95@gmail.com>
2020-11-17 03:55:55 +0100

    minimal

--- web/app_adminpanel.js
+++ web/app_adminpanel.js
@@ -58,7 +58,7 @@ app.use(bodyParser.urlencoded({
 app.use(cookieParser())
 app.use(favicon('Public/logo.png'));
 app.use('/public' , express.static(path.join(__dirname, 'public')));
-aapp.set('views', path.join(__dirname, '/web/views'))
+app.set('views', path.join(__dirname, '/web/views'))
 app.use(function(req, res, next) {
     res.header("Access-Control-Allow-Origin", "*");
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, Authorization,authtoken");
